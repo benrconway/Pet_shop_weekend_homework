a9ecc7c458d352f0bb5035c6af1a22ec1aa4ac7a
Benjamin Conway <benrconway84@gmail.com>
2017-08-04 16:40:40 +0100

    Upload homework as of Friday afternoon

---
+++ pet_shop.rb
@@ -0,0 +1,62 @@
+def pet_shop_name(name)
+  return name[:name]
+end
+
+def total_cash(account)
+  return account[:admin][:total_cash]
+end
+
+def add_or_remove_cash(account, cash)
+  return account[:admin][:total_cash] += cash
+end
+
+def pets_sold(shop_identifier)
+  return shop_identifier[:admin][:pets_sold]
+end
+
+def increase_pets_sold(shop, sales)
+  return shop[:admin][:pets_sold] += sales
+end
+
+def stock_count(shop)
+  return shop[:pets].length()
+end
+
+def pets_by_breed(shop,breed_name)
+  pets = []
+  for pet in shop[:pets] do
+    if  pet[:breed] == breed_name
+      pets.push(pet[:breed])
+    end
+  end
+return pets
+end
+
+def find_pet_by_name(shop, pet_name)
+  for pet in shop[:pets] do
+    if pet[:name] == pet_name
+      return pet
+    end
+  end
+return nil
+end
+
+def remove_pet_by_name(shop, pet_name)
+  for pet in shop[:pets] do
+    if pet[:name] == pet_name
+      shop[:pets].delete(pet)
+      end
+  end
+end
+
+def add_pet_to_stock(shop, new_stock)
+  shop[:pets].push(new_stock)
+end
+
+def customer_pet_count(customer)
+  return customer[:pets].count()
+end
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets].push(new_pet)
+end
